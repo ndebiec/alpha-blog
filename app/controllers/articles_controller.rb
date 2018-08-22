c5419b911046612b29f3e0e5b6570ecfa8e16e89
Nadia Dębiec <nadi337@gmail.com>
2018-08-22 09:28:44 +0000

    Create, add and show work well

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,21 @@
 class ArticlesController < ApplicationController
   
-  
   def new
     @article = Article.new
   end 
   
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
   
-  
-  def articles_show
+  def show
+    @article = Article.find(params[:id])
   end
   
   
