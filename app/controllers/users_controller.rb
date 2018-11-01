ccbd17641dbc7ea90d57e4e4c10f1b2c30fc3820
Nadia Dębiec <nadi337@gmail.com>
2018-11-01 17:39:25 +0000

    Added sign in upon sigh up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,9 @@ before_action :require_same_user, only: [:edit, :update]
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to alpha_blog #{@user.username}!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else render 'new'
     end
   end
