c7fe76cc5300eda788ffdae7658fe23023e17574
Nadia Dębiec <nadi337@gmail.com>
2018-11-05 00:43:57 +0000

    Added edit categories feature and created some partials to avoid redundancy

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
 
+before_action :set_category, only: [:edit, :update, :show, :destroy]
 before_action :require_admin, except: [:index, :show]
 
 def new
@@ -20,15 +21,29 @@ def index
   @categories=Category.all
 end
 
+def edit
+end
+
+def update
+  if @category.update(category_params)
+    flash[:success] = "Category name succesfully updated"
+    redirect_to category_path(@category)
+  else
+    render 'edit'
+  end
+end
 
 def show
-  @category=Category.find(params[:id])
 end
 
 private
 
   def category_params
     params.require(:category).permit(:name)
+  end
+  
+  def set_category
+    @category=Category.find(params[:id])
   end
 
   def require_admin
